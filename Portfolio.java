5573a2a0b011979b8d0bf93d46b444be677901f1
tknapp29 <tj29knap@siena.edu>
2023-02-17 19:31:57 -0500

    working on sellStock method

--- Portfolio.java
+++ Portfolio.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.Scanner; 
 /**
  * Write a description of class Portfolio here.
  *
@@ -7,18 +8,72 @@
  */
 public class Portfolio
 {
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // StringBuffer sb = new StringBuffer();
-        // sb.append(String.format("%6s%25s%10s%15s%15s%n", 
-                // "Symbol", "Name", "Shares", "@Price", "Total"));
-        // sb.append("-----------------------------------------------------------------------\n");
+    private ArrayList<StockHolding> stocks;
+    private int lifeTime;
+    private int lifeTimePayout;
 
-        // for(StockHolding s : stocks){
-            // sb.append(s.toString());
-        // }
-        // return sb.toString();
-    // }
+    public Portfolio()
+    {
+        stocks = new ArrayList<StockHolding>();
+        this.lifeTime = 0;
+        this.lifeTimePayout = 0;
+    }
+
+    public int getLifeTime()
+    {
+        return lifeTime;
+    }
+
+    public int lifeTimePayout()
+    {
+        return lifeTimePayout;
+    }
+
+    public int buyStock(String symbol, String name, int numShares,int pricePerShare)
+    {
+        int finalPrice = 0;
+        if(stocks.contains(getIndex(symbol)))
+        {
+            finalPrice = numShares*pricePerShare;
+            lifeTime = lifeTime + finalPrice;
+        }
+        else
+        {
+            StockHolding newStock = new StockHolding(symbol, name, numShares, pricePerShare);
+            stocks.add(newStock);
+        }
+        return finalPrice;
+    }
+
+    public int sellStock(String symbol, int sharesToSell)
+    {
+        return -1;
+    }
+
+    private int getIndex(String symbol)
+    {
+        for(StockHolding s : stocks)
+        {
+            if(s.equals(symbol))
+            {
+                return stocks.indexOf(symbol);
+            }
+        }    
+        return -1;
+    }
+
+    @Override
+    public String toString()
+    {
+        //DO NOT EDIT THIS METHOD.
+        StringBuffer sb = new StringBuffer();
+        sb.append(String.format("%6s%25s%10s%15s%15s%n", 
+                "Symbol", "Name", "Shares", "@Price", "Total"));
+        sb.append("-----------------------------------------------------------------------\n");
+
+        for(StockHolding s : stocks){
+            sb.append(s.toString());
+        }
+        return sb.toString();
+    }
 }
--- StockHolding.java
+++ StockHolding.java
@@ -1,16 +1,99 @@
+import java.util.Scanner;
 /**
- * Write a description of class Asset here.
+ * This class will hold information about a stock.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Tyler
+ * @version 2/23/23
  */
 public class StockHolding
 {
-    // @Override
-    // public String toString()
-    // {
-        // //DO NOT EDIT THIS METHOD.
-        // return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
-        // symbol, name, numShares, price, numShares * price); 
-    // }
+    Scanner input = new Scanner(System.in);
+    private String symbol;
+    private String name;
+    private int numShares;
+    private int price;
+    public StockHolding(String symbol, String name, int numShares, int price) {
+        this.symbol = symbol;
+        this.name = name;
+        this.numShares = numShares;
+        this.price = price;
+    }
+
+    /**
+     * Returns the stock symbol
+     * 
+     * @return symbol
+     * 
+     */
+    public String getSymbol() {
+        return symbol;
+    }
+
+    /**
+     * Returns the stock name
+     * 
+     * @return name
+     * 
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the number of stock shares
+     * 
+     * @return number of stock shares
+     * 
+     */
+    public int getNumShares() {
+        return numShares;
+    }
+
+    /**
+     * Returns the stock price
+     * 
+     * @return the stock price
+     * 
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * Determines the price to buy a share
+     * 
+     * @param shares, price per share
+     * @return price to buy shares
+     * 
+     */
+    public int buyShares(int shares, int pricePerShare)
+    {
+        return shares*pricePerShare;
+    }
+
+    /**
+     * Determines price for selling shares
+     * 
+     * @param shares
+     * @return symbol
+     * 
+     */
+    public int sharesToSell(int shares)
+    {
+        return shares*price;
+    }
+
+    /**
+     * Returns the method to string
+     * 
+     * @return toString
+     * 
+     */
+    @Override
+    public String toString()
+    {
+        //DO NOT EDIT THIS METHOD.
+        return String.format("%6s%25s%,10d  $%,12.2f  $%,12.2f%n", 
+            symbol, name, numShares, price, numShares * price); 
+    }
 }
